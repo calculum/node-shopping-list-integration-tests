570ddcd8e77b02bfd102c008b216c29abe82c7da
bo <clouculum@gmail.com>
2019-01-22 08:31:39 -0700

    update

--- recipesRouter.js
+++ recipesRouter.js
@@ -62,20 +62,20 @@ router.put("/:id", (req, res) => {
       return res.status(400).send(message);
     }
   }
-  if (req.params.id !== req.body.id) {
-    const message = `Request path id (${
-      req.params.id
-    }) and request body id ``(${req.body.id}) must match`;
-    console.error(message);
-    return res.status(400).send(message);
-  }
+  // if (req.params.id !== req.body.id) {
+  //   const message = `Request path id (${
+  //     req.params.id
+  //   })`;
+  //   console.error(message);
+  //   return res.status(400).send(message);
+  // }
   console.log(`Updating shopping list item \`${req.params.id}\``);
   Recipes.update({
     id: req.params.id,
     name: req.body.name,
     ingredients: req.body.ingredients
   });
-  res.status(200).end();
+  res.status(204).end();
 });
 
 module.exports = router;
--- test/test-for-recipes.js
+++ test/test-for-recipes.js
@@ -69,10 +69,10 @@ describe("Recipes", function () {
             })
 
             .then(function(res) {
-                expect(res).to.have.status(200);
-                expect(res).to.be.json;
-                expect(res,body).to.be.a("object");
-                expect(res.body).to.deep.euqal(updateData);
+                expect(res).to.have.status(204);
+                // expect(res).to.be.json;
+                // expect(res.body).to.be.a("object");
+                // expect(res.body).to.deep.equal(updateData);
             })
         );
     });
